f575d9872cd3a73f7e488c8f58f1a215d7dbc42b
Shreyash Singh <Rohitsingh9450@hotmail.com>
2022-12-25 19:09:13 +0530

    Project Change

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
       >
         <ProjectItem name={"React Piano"} img={'/p1.PNG'} code={"https://github.com/shreyashsingh-101/Piano"} live={"https://ssreact-piano.netlify.app"} description={'A beautiful piano created using React, HTML and CSS which is mobile device friendly and can be played using touch and keyboard.'} />
 
-        <ProjectItem name={"The Coders Adda"} img={'/p2.PNG'} code={"https://github.com/shreyashsingh-101/Hack-Nagpur"} live={""} description={"One Stop Destination for all developers to get resources, team and productivity manager."} />
+        <ProjectItem name={"The Memory Game"} img={'/p2.PNG'} code={"https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2010"} live={"https://shreyashsingh.me/Devsnest-Tasks/THA%20Day%2010"} description={"A mobile-friendly, JavaScript based game that puts your memory to test"} />
 
         <ProjectItem name={"React meme Generator"} img={'/p3.PNG'} code={"https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2022/day22"} live={"https://confident-tereshkova-2c7135.netlify.app"} description={"React based meme generator which uses imgflip API."} />
 
