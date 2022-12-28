b8807b8ec6b1ec398568a19f6d21a083be1c81cd
Shreyash Singh <Rohitsingh9450@hotmail.com>
2022-12-28 21:12:45 +0530

    Added Carousel to Projects Section

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,27 +1,19 @@
 "use client";
 
 import { motion } from "framer-motion";
-import {
-  slideIn,
-  textVariant,
-  fadeIn,
-} from "../utils/motion";
+import { slideIn, textVariant, fadeIn } from "../utils/motion";
 
-import { isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 
-
-
-const ProjectItem = ({ name, img, description, code, live}) => {
+const ProjectItem = ({ name, img, description, code, live }) => {
   return (
     <motion.div
       variants={fadeIn("left", "spring", 0.5, 1.5)}
       initial="hidden"
       whileInView="show"
-      className="rounded-xl overflow-hidden cursor-pointer w-3/4 md:w-2/3 "
+      className="rounded-xl overflow-hidden cursor-pointer md:w-[800px] w-[300px] shadow-2xl"
       style={{
-        minHeight: "300px",
-        height: "auto",
-        margin: "20px",
+        minHeight: "400px",
         background: `url(${img})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -31,8 +23,8 @@ const ProjectItem = ({ name, img, description, code, live}) => {
       <motion.div
         initial={isMobile ? "show" : "hidden"}
         whileHover="show"
-        style={{ minHeight: "300px" }}
-        className="flex flex-col justify-center items-center p-6 md:backdrop-brightness-50 backdrop-brightness-[.2] hover:backdrop-brightness-[.2] rounded-xl overflow-hidden cursor-pointer"
+        style={{ minHeight: "400px" }}
+        className="flex flex-col justify-center items-center md:p-16 p-4 md:backdrop-brightness-50 backdrop-brightness-[.2] hover:backdrop-brightness-[.2] rounded-xl overflow-hidden cursor-pointer"
       >
         <h1
           className="md:text-3xl text-xl font-bold text-white"
@@ -42,7 +34,7 @@ const ProjectItem = ({ name, img, description, code, live}) => {
         </h1>
         <motion.div
           variants={textVariant(0.3)}
-          className="md:text-xl text-sm text-white"
+          className="md:text-xl mt-6 text-sm text-white"
         >
           {description}
         </motion.div>
--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -2,31 +2,118 @@
 
 import { motion } from "framer-motion";
 import ProjectItem from "../components/ProjectItem";
+import { config } from "react-spring";
+import dynamic from "next/dynamic";
+const Carousel = dynamic(() => import("react-spring-3d-carousel"), {
+  ssr: false,
+});
 
 import styles from "../styles";
 import { staggerContainer } from "../utils/motion";
+import { useState, useRef } from "react";
 
 const Projects = () => {
+  const [goToSlide, setGoToSlide] = useState(0);
+
+  const [xDown, setXDown] = useState(null);
+
+  const myRef = useRef(null);
+
+  const handleTouchStart = (e) => {
+    const firstTouch = e.touches[0];
+    setXDown(firstTouch.clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    if (!xDown) {
+      return;
+    }
+
+    const xUp = e.touches[0].clientX;
+    const xDiff = xDown - xUp;
+
+    if (xDiff > 0) {
+      setGoToSlide(goToSlide + 1);
+    } else {
+      setGoToSlide(goToSlide - 1);
+    }
+
+    setXDown(null);
+  };
+
+  let slides = [
+    {
+      key: 0,
+      content: (
+        <ProjectItem
+          name={"React Piano"}
+          img={"/p1.PNG"}
+          code={"https://github.com/shreyashsingh-101/Piano"}
+          live={"https://ssreact-piano.netlify.app"}
+          description={
+            "A beautiful piano created using React, HTML and CSS which is mobile device friendly and can be played using touch and keyboard."
+          }
+        />
+      ),
+    },
+
+    {
+      key: 1,
+      content: (
+        <ProjectItem
+          name={"The Memory Game"}
+          img={"/p2.PNG"}
+          code={
+            "https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2010"
+          }
+          live={"https://shreyashsingh.me/Devsnest-Tasks/THA%20Day%2010"}
+          description={
+            "A mobile-friendly, JavaScript based game that puts your memory to test"
+          }
+        />
+      ),
+    },
+
+    {
+      key: 2,
+      content: (
+        <ProjectItem
+          name={"React meme Generator"}
+          img={"/p3.PNG"}
+          code={
+            "https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2022/day22"
+          }
+          live={"https://confident-tereshkova-2c7135.netlify.app"}
+          description={"React based meme generator which uses imgflip API."}
+        />
+      ),
+    },
+  ].map((slide, index) => {
+    return { ...slide, onClick: () => setGoToSlide(index) };
+  });
+
   return (
     <section id="projects">
-      <motion.h1
-        className={"text-4xl text-white mt-32 text-center font-extralight mb-16 pt-10"}
-      >PROJECTS</motion.h1>
+      <motion.h1 className={"text-4xl text-white m-auto text-center w-80%"}>
+        PROJECTS
+      </motion.h1>
 
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex flex-col items-center`}
+        className={`mx-auto flex flex-col items-center h-[600px] w-[100%]`}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
       >
-        <ProjectItem name={"React Piano"} img={'/p1.PNG'} code={"https://github.com/shreyashsingh-101/Piano"} live={"https://ssreact-piano.netlify.app"} description={'A beautiful piano created using React, HTML and CSS which is mobile device friendly and can be played using touch and keyboard.'} />
-
-        <ProjectItem name={"The Memory Game"} img={'/p2.PNG'} code={"https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2010"} live={"https://shreyashsingh.me/Devsnest-Tasks/THA%20Day%2010"} description={"A mobile-friendly, JavaScript based game that puts your memory to test"} />
-
-        <ProjectItem name={"React meme Generator"} img={'/p3.PNG'} code={"https://github.com/shreyashsingh-101/Devsnest-Tasks/tree/master/THA%20Day%2022/day22"} live={"https://confident-tereshkova-2c7135.netlify.app"} description={"React based meme generator which uses imgflip API."} />
-
-        
+        <Carousel
+          slides={slides}
+          goToSlide={goToSlide}
+          offsetRadius={5}
+          showNavigation={false}
+          animationConfig={config.slow}
+        />
       </motion.div>
     </section>
   );
