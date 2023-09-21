eeceb3d47febfb1c43326911543967d0d0c3d480
Shreyash Singh <rohitsingh9450@hotmail.com>
2023-09-21 11:36:51 +0530

    metadata

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,19 @@ function MyApp({ Component, pageProps }) {
         <title>Shreyash Singh</title>
         <meta
           name="description"
-          content="Portfolio of Shreyash Singh | Web Developer | Web Designer"
+          content="Portfolio of Shreyash Singh | Web Developer "
+        />
+        <meta
+          name="keywords"
+          content="shreyash singh 101, shreyashsingh-101, portfolio, shreyash, shreyash singh, shreyash singh portfolio, shreyash portfolio, shreyash singh web developer, shreyash singh web developer portfolio, shreyash singh web developer portfolio, react, node, nextjs, javascript, html, css, web developer, web developer portfolio, web"
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
       </Head>
 
       <Component {...pageProps} />
